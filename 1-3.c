afcb1834953b4de697310155e3e697fbf08f7f65
Yungan2313 <65326996+Yungan2313@users.noreply.github.com>
2022-09-28 20:07:15 +0800

    Create 1-3.c

---
+++ 1-3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a,b,c,d,e;
+    scanf("%d-%d-%d-%d-%d",&a,&b,&c,&d,&e);
+    printf("%03d\n",a);
+    printf("%03d\n",b);
+    printf("%03d\n",c);
+    printf("%d\n",d);
+    printf("%03d\n",e);
+    return 0;
+}
