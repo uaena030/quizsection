7674059b135772a9ba2bb55ade80593314fe17cb
Yungan2313 <65326996+Yungan2313@users.noreply.github.com>
2022-09-28 20:46:05 +0800

    Create tempCodeRunnerFile.c

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a[16];
+    for(int i=0; i<16; i++){
+        scanf("%d",a[i];)
+    }
+    for(int i=0; i<16; i++){
+        printf("%d\t",a[i]);
+    }
+
+
+}
