d9b04d6426d93930f669a7e2099987d9a4e83f44
Yungan2313 <65326996+Yungan2313@users.noreply.github.com>
2022-09-28 20:45:49 +0800

    Update 1-2.c

--- 1-2.c
+++ 1-2.c
@@ -6,9 +6,6 @@ int main(){
     scanf("%d %f %d/%d/%d",&a,&b,&c,&d,&e);
     printf("Item\tUnit\tPurchase\n");
     printf("\tPrice\tDate\n");
-    printf("%d\t$%.2f\t%d\\%d\\%04d",a,b,c,d,e);
-    // printf("%*s %*s %*s\n","Item","Unit","Purchase");
-    // printf("%*s %*s %*s\n"," ","Price","Date");
-    // printf("%*d %*.2f  %d\\%d\\%d",a,b,c,d,e);
+    printf("%d\t$%.2f\t%02d\\%02d\\%04d",a,b,c,d,e);
 
 }
