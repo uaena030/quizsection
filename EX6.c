868514f1ab97ec0328491ead9001e472ac165a5b
Yungan2313 <65326996+Yungan2313@users.noreply.github.com>
2022-11-09 21:06:18 +0800

    wifi 炸裂

--- EX6.c
+++ EX6.c
@@ -3,36 +3,48 @@
 
 int main(){
     char i,a,A;
-    int bytescount = 0,lowercount = 0,uppercount = 0,dcount = 0,wcount = 0,linecount = 0;
-    int alph[26],alphsum = 0;
-    float rate;
+    int bytescount = 0,lowercount = 0,uppercount = 0,dcount = 0,wcount = 0,linecount = 1;
+    int alph[26],last = 0;
+    float rate,alphsum = 0;
     for(int i = 0;i<26;i++){
         alph[i] = 0;
     }
     while((i = getchar()) != EOF){
         bytescount += 1;
         if(i == '\n'){
-            lincount += 1;
-        }
-        else if(i == ' '){
+            linecount += 1;
             wcount += 1;
+            last = 1;
+        }
+        else if(i == ' ' || i == '\t' || i == '\r' || i == '\f' || i == '\v'){
+            wcount += 1;
+            last = 0;
         }
         else if(i <= 'Z' && i >= 'A'){
             alph[i - 'A'] += 1;
             uppercount += 1;
+            last = 0;
         }
-        else if(i <= 'a' && i >= 'z'){
+        else if(i >= 'a' && i <= 'z'){
             alph[i - 'a'] += 1;
             lowercount += 1;
+            last = 0;
         }
         else if(i >= '0' && i <= '9'){
             dcount += 1;
+            last = 0;
         }
+        else{
+            last = 0;
+        }
+    }
+    if(last){
+        linecount -= 1;
     }
     printf("Bytes Count: %d\n",bytescount);
     printf("Lowercase Count: %d\n",lowercount);
     printf("Uppercase Count: %d\n",uppercount);
-    printf("Digit Count: %d\n",digitcount);
+    printf("Digit Count: %d\n",dcount);
     printf("Whitespace Count: %d\n",wcount);
     printf("Line Count: %d\n",linecount);
     for(i=0; i<26; i++){
@@ -40,9 +52,9 @@ int main(){
     }
     for(i = 0; i <26;i++){
         A = 'A' + i;
-        a = tolower(a);
+        a = tolower(A);
         rate = alph[i]/alphsum;
-        printf("%c/%c: %d,%d",a,A,alph[i],rate);
+        printf("%c/%c: %d, %.3f\n",A,a,alph[i],rate);
     }
 }
 
